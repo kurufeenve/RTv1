6ef61eaa82de9fe001be93a7b4e4bc161846feb1
Vladyslav ORDYNSKYI <vordynsk@e1r2p12.unit.ua>
2018-09-19 17:29:27 +0300

    git script

--- git.sh
+++ git.sh
@@ -7,8 +7,8 @@ read -p 'answer: ' answer
 if [[ "$answer" == "y" ]]
 then
 	read -p "comment: " comment
-	git commit -m $comment
+	git commit -m "$comment"
 	git push
-#else
-#	echo you typed something
+else
+	echo Cancelling . . .
 fi
