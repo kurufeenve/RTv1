51c9dd857aa841a7bf17d72e633ad3e43862b2d5
Vladyslav ORDYNSKYI <vordynsk@e3r7p8.unit.ua>
2018-10-04 22:44:33 +0300

    atoi_base

--- libft/ft_atoi_base.c
+++ libft/ft_atoi_base.c
@@ -9,10 +9,10 @@
 /*   Updated: 2018/10/03 21:38:39 by vordynsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+#include <stdio.h>
 #include "includes/libft.h"
 
-static int		base(char c, int base)
+static int		base_check(char c, int base)
 {
 	char		buff[17];
 	int			i;
@@ -30,15 +30,35 @@ static int		base(char c, int base)
 int				ft_atoi_base(char const *str, int base)
 {
 	int		i;
+	int		s;
 	size_t	num;
 
 	i = 0;
+	s = 1;
 	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
 		str[i] == '\r' || str[i] == '\v' || str[i] == '\f')
 		i++;
-	num = 0;
-	while (base(str[i], base) > 0)
+	if ((str[i] == '+' || str[i] == '-') && base == 10)
 	{
-		num = (num * base) + (str[i++] - 48);
+		if (str[i] == '-')
+			s = -1;
+		i++;
 	}
+	num = 0;
+	while (base_check(str[i], base) > 0)
+	{
+		if (str[i] > 47 && str[i] < 58)
+			num = (num * base) + (str[i++] - 48);
+		else
+			num = (num * base) + (str[i++] - 55);
+	}
+	return (((int)num * s));
+}
+
+int		main(void)
+{
+	char	s[] = "256";
+	int		i;
+
+	print("");
 }
